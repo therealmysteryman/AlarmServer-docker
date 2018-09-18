12d07d704fd7f533953dfd8112445a3890f58ccf
automationgeek <jf.tremblay@gmail.com>
2018-09-18 17:22:26 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 sed -i 's/envisalinkIP/$EVL_IP/g' /var/AlarmServer-master/alarmserver.cfg
 sed -i 's/user/$PASSWORD/g' /var/AlarmServer-master/alarmserver.cfg
 sed -i 's/usercode/$USERCODE/g' /var/AlarmServer-master/alarmserver.cfg
-/usr/bin/python /var/AlarmServer-master/alarmserver.py -c /var/AlarmServer-master/alarmserver.cfg
+python /var/AlarmServer-master/alarmserver.py -c /var/AlarmServer-master/alarmserver.cfg
