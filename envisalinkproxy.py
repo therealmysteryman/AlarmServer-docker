8b1742cf246d40374b81c4d86f0a9e3d1b7c2564
automationgeek <jf.tremblay@gmail.com>
2018-09-18 18:03:38 -0400

    Update envisalinkproxy.py

--- envisalinkproxy.py
+++ envisalinkproxy.py
@@ -18,8 +18,8 @@ class Proxy(object):
             proxy.listen(config.ENVISALINKPROXYPORT)
 
 class ProxyServer(TCPServer):
-    def __init__(self, ssl_options=None, **kwargs):
-        TCPServer.__init__(self, io_loop=io_loop, ssl_options=ssl_options, **kwargs)
+    def __init__(self, io_loop=io_loop,ssl_options=None, **kwargs):
+        TCPServer.__init__(self,ssl_options=ssl_options, **kwargs)
         self.connections = {}
         events.register('proxy', self.proxy_event)
 
